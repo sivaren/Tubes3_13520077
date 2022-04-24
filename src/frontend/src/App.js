5d06236470731ffa91b47a728e6fd1faa701bf59
ravaattar <rava.naufal2014@gmail.com>
2022-04-25 03:31:44 +0700

    added react-router-dom, history

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="">
      <Hero />
      <Navbar />
-     <Test />
+     {/* <Test /> */}
     </div>
   );
 }
--- src/frontend/src/components/Navbar/Navbar.js
+++ src/frontend/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import './Navbar.css'
+// import { Link } from 'react-router-dom'
 
 function Navbar() {
 
