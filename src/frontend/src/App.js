4ef4e46eed692dbebcb133c29be77a600d37f1c8
ravaattar <rava.naufal2014@gmail.com>
2022-04-29 12:50:41 +0700

    tidy up frontend

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,18 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
-import MyTest from './pages/MyTest'
-import Home from './pages/Home/Home'
-import Add from './pages/Add/Add'
-import Test from './pages/Test/Test'
-import Result from './pages/Result/Result'
-import Hero from './components/Hero/Hero'
-import Navbar from './components/Navbar/Navbar'
+import './App.css';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Add from './pages/Add/Add';
+import Test from './pages/Test/Test';
+import Result from './pages/Result/Result';
 
 function App() {
-
   return (
-    <div className="">
+    <div>
      <Hero />
      <Navbar />
      <Routes>
